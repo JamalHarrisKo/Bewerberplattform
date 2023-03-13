3a968b4a02ef5e12bdbdbf46f4975340fe518a3a
jamal harris <jamal.c.harris@gmx.de>
2023-03-13 23:42:38 +0100

    initial

---
+++ Bewerberplattform/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+
+    <!-- Add Navigation via router Links -->
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <span>-</span>
+      <RouterLink to="/jobs">Jobs</RouterLink>
+      <span>-</span>
+      <RouterLink to="/Ausbildung">Ausbildung</RouterLink>
+    </nav>
+
+  </header>
+  <!-- Render content with router -->
+  <RouterView />
+</template>
+
+<style scoped></style>
